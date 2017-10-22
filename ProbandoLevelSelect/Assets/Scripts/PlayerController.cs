c304bf946fe16b79881e6554bdb10f9f04015453
devildrake <divadarrei@gmail.com>
2017-10-22 13:31:46 +0200

    Cambiado el movimiento para detectar si se esta grounded o no con Triggers2D (HAY QUE PONERLE EL TAG GROUND A LOS OBJETOS QUE SE CONSIDERAN PLATAFRMA) Tambien se ha ajustado la ralentización en el aire, ahora funciona como se quiere y con las inputs de Horizontal (Se usa GetAxisRaw())

--- ProbandoLevelSelect/Assets/Scripts/PlayerController.cs
+++ ProbandoLevelSelect/Assets/Scripts/PlayerController.cs
@@ -20,23 +20,32 @@ public class PlayerController : MonoBehaviour {
     //Referencia al RigidBody2D del personaje, se inicializa en el start.
     Rigidbody2D rb;
 
+    //BOOLEANO QUE GESTIONA SI EL TIEMPO ESTA REDUDCIDO O NO
     bool slowMotion;
+
+    //ESTE INT ES -1 SI EL MOVIMIENTO PREVIO FUE HACIA LA IZQUIERDA Y ES 1 SI EL MOVIMIENTO PREVIO FUE HACIA LA DERECHA
+    float prevHorizontalMov;
+
+    //Booleano que comprueba si el personaje deberia estar ralentizado en el aire, se reinicia al estar en el suelo (Que se comprueba con colliders2D y tags)
+    bool slowedInTheAir;
 
     //Se inicializan las cosas
     void Start() {
         groundMask = LayerMask.GetMask("Ground");
         rb = GetComponent<Rigidbody2D>();
         slowMotion = false;
+        slowedInTheAir = false;
     }
 
     // Update is called once per frame
     void Update() {
-        CheckGrounded();
+        CheckNotGrounded();
         Move();
         CheckInputs();
         CheckSlowMotion();
     }
 
+    //Método que comprueba si se ha pulsado la tecla control y cambia el booleano slowMotion además de modificar el timescale
     void CheckSlowMotion() {
         if ((Input.GetKeyDown(KeyCode.LeftControl)) && !slowMotion) {
             Time.timeScale = 0.5f;
@@ -48,19 +57,35 @@ public class PlayerController : MonoBehaviour {
         }
     }
 
-    //Funcion que hace al personaje moverse hacia los lados a partir del input de las flechas, en caso de estar en el aire se mantiene siempre y cuando no se pulsen de nuevo las teclas A o D lo que hace que se reduzca a la mitad la velocidad de movimiento en x 
+    //Funcion que hace al personaje moverse hacia los lados a partir del input de las flechas, en caso de estar en el aire se mantiene siempre y cuando se cambie la dirección horizontal en el aire se reduce a la mitad la velocidad
     void Move() {
+        bool changed = false;
+        float mustSlow = 1;
+        if (Input.GetAxisRaw("Horizontal") != (float)prevHorizontalMov) {
+            changed = true;
+            prevHorizontalMov = Input.GetAxisRaw("Horizontal");
+        }
 
-
-        if (grounded)
+        if (grounded) {
             transform.Translate(Vector3.right * Input.GetAxis("Horizontal") * characterSpeed * Time.deltaTime);
-        else if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.A)) {
-            transform.Translate((Vector3.right * Input.GetAxis("Horizontal") * characterSpeed) / 2 * Time.deltaTime);
+            slowedInTheAir = false;
+        }
+        else if (changed) {
+
+            slowedInTheAir = true;
+            mustSlow = 0.5f;
+            transform.Translate(Vector3.right * Input.GetAxis("Horizontal") * characterSpeed * mustSlow * Time.deltaTime);
+
         }
 
         else {
-            transform.Translate(Vector3.right * Input.GetAxis("Horizontal") * characterSpeed * Time.deltaTime);
+            if (slowedInTheAir) {
+                mustSlow = 0.5f;
+            }
+            transform.Translate(Vector3.right * Input.GetAxis("Horizontal") * characterSpeed *mustSlow* Time.deltaTime);
+
         }
+
     }
 
     //Metodo que añade una fuerza al personaje para simular un salto
@@ -69,12 +94,26 @@ public class PlayerController : MonoBehaviour {
     }
 
     //Método que comprueba si la velocidad y del personaje es 0 o aprox. y actualiza el booleano grounded en consecuencia
-    void CheckGrounded() {
+    void CheckNotGrounded() {
         if (rb.velocity.y < 0.1f && rb.velocity.y>=0.0f || rb.velocity.y > -0.1f && rb.velocity.y <=0.0f) {
-            grounded = true;
+
         }
         else {
             grounded = false;
+        }
+    }
+
+
+    //OnTriggerStay2D y OnTriggerEnter2D que comprueban si el tag del trigger es ground para actualizar el booleano grounded
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.tag == "Ground") {
+            grounded = true;
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision) {
+        if (collision.tag == "Ground") {
+            grounded = true;
         }
     }
 
