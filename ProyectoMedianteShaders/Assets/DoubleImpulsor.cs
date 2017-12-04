ecebc297d083c9dcfba0658deeb9215559ba6382
Devildrake <divadarrei@gmail.com>
2017-12-04 11:39:36 +0100

    Introducida escena para probar el coth, introducido elemento que impulsa en dawn

---
+++ ProyectoMedianteShaders/Assets/DoubleImpulsor.cs
@@ -0,0 +1,197 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoubleImpulsor : DoubleObject
+{
+    // Use this for initialization
+    Rigidbody2D rb;
+    public LayerMask groundMask;
+    float distanciaBordeSprite;
+    public List<GameObject> inTriggerZoneObjects;
+    bool changed;
+    bool rising = true;
+    public float localGravityScale = -0.4f;
+    void Start()
+    {
+        InitTransformable();
+        isPunchable = false;
+        isBreakable = false;
+        interactuableBySmash = false;
+        offset = GameLogic.instance.worldOffset;
+        if (worldAssignation == world.DAWN)
+        {
+            GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Kinematic;
+            GetComponent<SpriteRenderer>().sprite = imagenDawn;
+        }
+        else
+        {
+            GetComponent<SpriteRenderer>().sprite = imagenDusk;
+
+        }
+
+
+        rb = GetComponent<Rigidbody2D>();
+        groundMask = LayerMask.GetMask("Ground");
+
+        distanciaBordeSprite = 0.745f;
+        rb.mass = 5000;
+    }
+
+    protected override void BrotherBehavior()
+    {
+        Vector3 positionWithOffset;
+        if (GetComponent<Rigidbody2D>().bodyType == RigidbodyType2D.Kinematic)
+        {
+            positionWithOffset = brotherObject.transform.position;
+
+            if (worldAssignation == world.DAWN)
+                positionWithOffset.y += offset;
+            else
+            {
+                positionWithOffset.y -= offset;
+            }
+
+            transform.position = positionWithOffset;
+            transform.rotation = brotherObject.transform.rotation;
+
+        }
+
+    }
+
+    void BecomePunchable()
+    {
+        isPunchable = true;
+    }
+
+    protected override void LoadResources()
+    {
+        if (worldAssignation == world.DAWN)
+        {
+            imagenDawn = Resources.Load<Sprite>("Presentacion/DawnSprites/DawnBox");
+        }
+        else
+        {
+            imagenDusk = Resources.Load<Sprite>("Presentacion/DuskSprites/DuskBox");
+
+        }
+    }
+
+    public override void Change()
+    {
+        changed = true;
+        //El objeto que modifica a ambos haciendo de controlador es el que pertenece a Dawn
+        if (worldAssignation == world.DAWN)
+        {
+            //Si antes del cambio estaba en dawn, pasara a hacerse kinematic y al otro dynamic, además de darle su velocidad
+            if (dawn)
+            {
+                dominantVelocity = GetComponent<Rigidbody2D>().velocity;
+                brotherObject.GetComponent<DoubleObject>().dominantVelocity = GetComponent<Rigidbody2D>().velocity;
+                brotherObject.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
+                GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Kinematic;
+                OnlyFreezeRotation();
+                brotherObject.GetComponent<Rigidbody2D>().velocity = dominantVelocity;
+                GetComponent<Rigidbody2D>().velocity = new Vector2(0.0f, 0.0f);
+            }
+            //Si antes del cambio estaba en dusk, pasara a hacerse dynamic y al otro kinematic, además de darle su velocidad 
+            else
+            {
+                dominantVelocity = brotherObject.GetComponent<Rigidbody2D>().velocity;
+                brotherObject.GetComponent<DoubleObject>().dominantVelocity = brotherObject.GetComponent<Rigidbody2D>().velocity;
+                GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
+                brotherObject.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Kinematic;
+                brotherObject.GetComponent<Rigidbody2D>().velocity = new Vector2(0.0f, 0.0f);
+                GetComponent<Rigidbody2D>().velocity = dominantVelocity;
+            }
+
+            dawn = !dawn;
+            brotherObject.GetComponent<Transformable>().dawn = !brotherObject.GetComponent<Transformable>().dawn;
+        }
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (worldAssignation == world.DAWN)
+        {
+            if (collision.gameObject.GetComponent<Rigidbody2D>() != null)
+            {
+                if (!inTriggerZoneObjects.Contains(collision.gameObject))
+                {
+                    inTriggerZoneObjects.Add(collision.gameObject);
+                }
+            }
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (worldAssignation == world.DAWN)
+        {
+            if (collision.gameObject.GetComponent<Rigidbody2D>() != null)
+            {
+                if (!inTriggerZoneObjects.Contains(collision.gameObject))
+                {
+                    inTriggerZoneObjects.Add(collision.gameObject);
+                }
+            }
+        }
+    }
+
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (worldAssignation == world.DAWN)
+        {
+            if (collision.gameObject.GetComponent<Rigidbody2D>() != null)
+            {
+                if (inTriggerZoneObjects.Contains(collision.gameObject))
+                {
+                    collision.gameObject.GetComponent<Rigidbody2D>().gravityScale = 1;
+                    inTriggerZoneObjects.Remove(collision.gameObject);
+                }
+            }
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (rising)
+        {
+            localGravityScale -= 0.3f * Time.deltaTime;
+            if (localGravityScale < -0.5f)
+            {
+                rising = false;
+            }
+        }
+        else
+        {
+            localGravityScale += 0.3f * Time.deltaTime;
+            if (localGravityScale > 0.5f) ;
+            {
+                rising = true;
+            }
+        }
+
+        AddToGameLogicList();
+        BrotherBehavior();
+        if (changed)
+        {
+            foreach (GameObject g in inTriggerZoneObjects)
+            {
+                g.GetComponent<Rigidbody2D>().gravityScale = 1;
+            }
+            inTriggerZoneObjects.Clear();
+            changed = false;
+        }
+
+        foreach (GameObject g in inTriggerZoneObjects){
+            g.GetComponent<Rigidbody2D>().gravityScale = localGravityScale;
+        }
+
+    }
+}
