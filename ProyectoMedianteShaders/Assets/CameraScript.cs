8787dd81257d6b67280b47226b977e8a8791065f
Devildrake <divadarrei@gmail.com>
2017-12-30 18:16:29 +0100

    Establecido un follow smoothed en el campo de pruebas

---
+++ ProyectoMedianteShaders/Assets/CameraScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class CameraScript : MonoBehaviour {
+
+    public Transform target;
+    public Vector3 offset;
+    public float smoothSpeed = 10.0f;
+
+    private void FixedUpdate() {
+        Vector3 desiredPosition = target.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed*Time.deltaTime);
+        transform.position = smoothedPosition;
+        //transform.LookAt(target.position);
+
+
+    }
+
+}
