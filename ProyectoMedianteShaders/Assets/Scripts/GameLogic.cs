2d3801ac5ac6704714607200adf50bf24511c8fb
Alex <a.canutperdigon@gmail.com>
2018-02-13 11:52:56 +0100

    Añadidas las variables para cambiar las opciones del juego.

--- ProyectoMedianteShaders/Assets/Scripts/GameLogic.cs
+++ ProyectoMedianteShaders/Assets/Scripts/GameLogic.cs
@@ -85,8 +85,16 @@ public class GameLogic : MonoBehaviour {
 
     public float worldOffset;
 
-    public float musicVolume;
-    public float sfxVolume;
+    public float musicVolume; //modificador del volumen de la musica
+    public float sfxVolume; //modificador del volumen de los efectos de sonido
+    public int screenRefreshRate; //varia los Hz de refresco de la pantalla
+    public int maxFrameRate; //limita el framerate
+
+    /*
+     * Conjunto de metodos que se llamaran al cambiar las opciones de juego
+     */
+
+    //=====================FINAL DE METODOS DE OPCIONES========================
 
     public void KillPlayer() {
         currentPlayer.Kill();
