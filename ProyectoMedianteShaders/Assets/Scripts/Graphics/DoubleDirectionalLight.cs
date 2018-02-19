39035b42ac791d98774a4ec5f790145ed2aea208
Unknown <a.canutperdigon@gmail.com>
2018-02-19 18:34:30 +0100

    Iluminacion en el nivel 0-1. Estoy trasteando en un sistema de particulas para el bush de dusk.

---
+++ ProyectoMedianteShaders/Assets/Scripts/Graphics/DoubleDirectionalLight.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoubleDirectionalLight : DoubleObject {
+
+    public Color duskColor, dawnColor;
+    public float duskIntensity, dawnIntensity;
+
+	void Start () {
+        if (dawn) {
+            GetComponent<Light>().color = dawnColor;
+            GetComponent<Light>().intensity = dawnIntensity;
+        }
+        else {
+            GetComponent<Light>().color = duskColor;
+            GetComponent<Light>().intensity = duskIntensity;
+        }
+	}
+	
+	void Update () {
+        AddToGameLogicList();	
+	}
+
+    public override void Change() {
+        base.Change();
+
+        if (dawn) {
+            GetComponent<Light>().color = dawnColor;
+            GetComponent<Light>().intensity = dawnIntensity;
+        }
+        else {
+            GetComponent<Light>().color = duskColor;
+            GetComponent<Light>().intensity = duskIntensity;
+        }
+    }
+}
