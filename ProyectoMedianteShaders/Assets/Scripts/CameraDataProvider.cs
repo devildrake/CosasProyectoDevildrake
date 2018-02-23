38868db4ef2d780c9610292f611327633e9e32b5
Devildrake <divadarrei@gmail.com>
2018-02-23 16:29:14 +0100

    LOG: - Establecido un prefab que coloca la camara donde se le especifica y despues se auto destruye. (HAY QUE COLOCARLO EN LA POSICIÓN DESEADA PARA LA CAMARA ADEMÁS DE ESPECIFICAR LOS PARÁMETROS EN EL INSPECTOR

---
+++ ProyectoMedianteShaders/Assets/Scripts/CameraDataProvider.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraDataProvider : MonoBehaviour {
+
+    public float farDistance;
+    public float closeDistance;
+    public Vector3 offset;
+    CameraScript[] cameraScripts;
+
+    private void Awake() {
+        cameraScripts = new CameraScript[2];
+        GameObject camObj =  GameObject.Find("Cameras");
+        cameraScripts = camObj.GetComponentsInChildren<CameraScript>();
+        cameraScripts[0].farDistance = farDistance;
+        cameraScripts[0].closeDistance = closeDistance;
+        cameraScripts[0].transform.position = transform.position;
+        cameraScripts[0].offset = offset;
+        cameraScripts[0].OffsetX = offset.x;
+
+        cameraScripts[1].farDistance = farDistance;
+        cameraScripts[1].closeDistance = closeDistance;
+        cameraScripts[1].transform.position = transform.position;
+        cameraScripts[1].offset = offset;
+        cameraScripts[1].OffsetX = offset.x;
+
+        Destroy(gameObject);
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
