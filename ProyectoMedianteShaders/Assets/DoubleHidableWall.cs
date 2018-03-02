bdfaa4a21024e5f2f253a320a24958aa4a5ebf39
devildrake <divadarrei@gmail.com>
2018-03-02 22:07:45 +0100

    LOG: -Preparando el terreno para meter la Pared que se esconde (LAPIPAPEL)

---
+++ ProyectoMedianteShaders/Assets/DoubleHidableWall.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoubleHidableWall : DoubleObject {
+    public GameObject wall;
+    public GameObject trigger1;
+    public GameObject trigger2;
+
+
+    void Start() {
+        canBounce = true;
+        InitTransformable();
+        isPunchable = true;
+        isBreakable = false;
+        interactuableBySmash = false;
+        offset = GameLogic.instance.worldOffset;
+        if (worldAssignation == world.DAWN) {
+            GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Kinematic;
+            //GetComponent<SpriteRenderer>().sprite = imagenDawn;
+        } else {
+            //GetComponent<SpriteRenderer>().sprite = imagenDusk;
+
+        }
+
+    }
+
+    protected override void BrotherBehavior() {
+        Vector3 positionWithOffset;
+        positionWithOffset = brotherObject.transform.position;
+
+        if (worldAssignation == world.DAWN)
+            positionWithOffset.y += offset;
+        else {
+            positionWithOffset.y -= offset;
+        }
+
+        transform.position = positionWithOffset;
+        transform.rotation = brotherObject.transform.rotation;
+    }
+
+    protected override void LoadResources() {
+
+    }
+
+    public override void Change() {
+            dawn = !dawn;
+
+    }
+
+
+    // Update is called once per frame
+    void Update() {
+        AddToGameLogicList();
+        BrotherBehavior();
+
+        //if()
+
+    }
+}
