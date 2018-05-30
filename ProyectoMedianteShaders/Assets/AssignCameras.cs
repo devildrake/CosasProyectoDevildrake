ca2cf843639c0609952bde4217b8c43a4f9c0e67
Yodeverdad <a.canutperdigon@gmail.com>
2018-05-30 17:16:51 +0200

    Arreglada asignación de cambio de post processing behavior.

---
+++ ProyectoMedianteShaders/Assets/AssignCameras.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.PostProcessing;
+
+public class AssignCameras : MonoBehaviour {
+    public PostProcessingBehaviour[] ppp;
+	// Use this for initialization
+	void Start () {
+        GameLogic.instance.ppp = ppp;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
