d8377ded3c3c33c87fdc214ffa74337081e95c45
Devildrake <divadarrei@gmail.com>
2018-06-01 13:47:36 +0200

    Establecido el script que se encarga de que las imagenes varien en función del idioma, aun no se usa en ningun lado

---
+++ ProyectoMedianteShaders/Assets/Scripts/ImageLanguage.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ImageLanguage : MonoBehaviour {
+    public MessagesFairy.LANGUAGE prevLanguage;
+    public Sprite EngText;
+    public Sprite SpaText;
+
+    Image text;
+    // Use this for initialization
+    void Start() {
+        text = GetComponent<Image>();
+        prevLanguage = MessagesFairy.LANGUAGE.None;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (GameLogic.instance != null) {
+            if (prevLanguage != GameLogic.instance.currentLanguage) {
+                prevLanguage = GameLogic.instance.currentLanguage;
+                if (GameLogic.instance.currentLanguage == MessagesFairy.LANGUAGE.English) {
+                    if (EngText != null) {
+                        text.sprite = EngText;
+                    }
+                } else {
+                    if (SpaText != null) {
+                        text.sprite = SpaText;
+                    }
+                }
+            }
+        }
+    }
+}
