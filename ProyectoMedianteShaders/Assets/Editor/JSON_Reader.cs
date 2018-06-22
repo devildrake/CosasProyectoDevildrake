d3c3cabe7429324336ec7aaa5daa6989d32afa74
Alex Canut <a.canutperdigon@gmail.com>
2018-06-22 14:01:39 +0200

    Ya funciona la tool para leer los json. Desde ahi puedes crear, ver y editarlos.

--- ProyectoMedianteShaders/Assets/Editor/JSON_Reader.cs
+++ ProyectoMedianteShaders/Assets/Editor/JSON_Reader.cs
@@ -2,22 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
+using System.IO;
 
 public class JSON_Reader : EditorWindow {
 
-    private string[] str;
-    private int items = 1;
-    private string path;
-
-    class JSON_Info {
-        public string key;
-        public string value;
-
-        public JSON_Info(string key, string value) {
-            this.key = key;
-            this.value = value;
-        }
-    }
+    public LocalizationData localizationData;
+    private Vector2 scrollPos;
 
     [MenuItem("Window/JSON")]
     public static void ShowWindow() {
@@ -25,15 +15,61 @@ public class JSON_Reader : EditorWindow {
     }
 
     private void OnGUI() {
-        path = Application.streamingAssetsPath;
-        str = new string[items];
-        GUILayout.Label("JSON items", EditorStyles.boldLabel);
-        //items = EditorGUILayout.IntField("Items", items);
+        if(localizationData != null) {
+            scrollPos = EditorGUILayout.BeginScrollView(scrollPos); //scroll view
+            {
+                SerializedObject serializedObject = new SerializedObject(this);
+                SerializedProperty serializedProperty = serializedObject.FindProperty("localizationData");
+                EditorGUILayout.PropertyField(serializedProperty, true);
+                serializedObject.ApplyModifiedProperties();
+                Debug.Log("Esto lo hace?");
+            }
+            EditorGUILayout.EndScrollView();
 
-        //for(int i = 0; i<items; i++) {
-        //    str[i] = EditorGUILayout.TextField("hey", str[i]);
-        //}
+            if (GUILayout.Button("SAVE")){
+                SaveData();
+            }
+        }
 
-        //JSON_Info i = JsonUtility.FromJson<JSON_Info>();
+        if (GUILayout.Button("LOAD")) {
+            LoadData();
+        }
+
+        if (GUILayout.Button("CREATE")) {
+            CreateNewData();
+        }
     }
+
+    private void CreateNewData() {
+        localizationData = new LocalizationData();
+    }
+
+    private void LoadData() {
+        string path = EditorUtility.OpenFilePanel("Selecciona el archivo", Application.streamingAssetsPath, "json");
+
+        if (!string.IsNullOrEmpty(path)) {
+            string jsonData = File.ReadAllText(path);
+            localizationData = JsonUtility.FromJson<LocalizationData>(jsonData);
+        }
+    }
+
+    private void SaveData() {
+        string path = EditorUtility.SaveFilePanel("Guardar", Application.streamingAssetsPath, "", "json");
+
+        if (!string.IsNullOrEmpty(path)) {
+            string jsonData = JsonUtility.ToJson(localizationData);
+            File.WriteAllText(path, jsonData);
+        }
+    }
+}
+
+[System.Serializable]
+public class LocalizationData {
+    public LocalizationItem[] items;
+}
+
+[System.Serializable]
+public class LocalizationItem {
+    public string key;
+    public string value;
 }
