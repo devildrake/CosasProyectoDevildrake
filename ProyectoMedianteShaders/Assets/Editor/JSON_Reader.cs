7ecfea7974992ea98d425b3fda79baea01c74373
Alex Canut <a.canutperdigon@gmail.com>
2018-08-28 13:20:15 +0200

    Añadidos los textos del menu opciones al json. Pequeños cambios en la tool para leer el json del idioma.

--- ProyectoMedianteShaders/Assets/Editor/JSON_Reader.cs
+++ ProyectoMedianteShaders/Assets/Editor/JSON_Reader.cs
@@ -8,10 +8,11 @@ public class JSON_Reader : EditorWindow {
 
     public LocalizationData localizationData;
     private Vector2 scrollPos;
+    private string fileNameOpened;
 
     [MenuItem("Tools/JSON Reader (Language)")]
     public static void ShowWindow() {
-        EditorWindow.GetWindow(typeof(JSON_Reader));
+        GetWindow(typeof(JSON_Reader),false, "JSON Reader");
     }
 
     private void OnGUI() {
@@ -41,36 +42,30 @@ public class JSON_Reader : EditorWindow {
 
     private void CreateNewData() {
         localizationData = new LocalizationData();
+        GetWindow(typeof(JSON_Reader)).titleContent = new GUIContent("JSON Reader");
     }
 
     private void LoadData() {
         string path = EditorUtility.OpenFilePanel("Selecciona el archivo", Application.streamingAssetsPath + "/lan", "json");
+        string[] s = path.Split(new char[] { '/', '.' });
+        fileNameOpened = s[s.Length - 2];
 
         if (!string.IsNullOrEmpty(path)) {
             string jsonData = File.ReadAllText(path);
             localizationData = JsonUtility.FromJson<LocalizationData>(jsonData);
+            GetWindow(typeof(JSON_Reader)).titleContent = new GUIContent(fileNameOpened + ".json");
         }
     }
 
     private void SaveData() {
         string path = EditorUtility.SaveFilePanel("Guardar", Application.streamingAssetsPath + "/lan", "", "json");
+        string[] s = path.Split(new char[] { '/', '.' });
+        fileNameOpened = s[s.Length - 2];
 
         if (!string.IsNullOrEmpty(path)) {
             string jsonData = JsonUtility.ToJson(localizationData);
             File.WriteAllText(path, jsonData);
+            GetWindow(typeof(JSON_Reader)).titleContent = new GUIContent(fileNameOpened + ".json");
         }
     }
 }
-
-/*
-[System.Serializable]
-public class LocalizationData {
-    public LocalizationItem[] items;
-}
-
-[System.Serializable]
-public class LocalizationItem {
-    public string key;
-    public string value;
-}
-*/
